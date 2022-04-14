82908a2da934949d870e95c1322e346061563f86
chy101010 <jason2028823@gmail.com>
2022-04-14 01:22:05 -0400

    filter bug fix

--- js/main.js
+++ js/main.js
@@ -570,7 +570,7 @@ let barHours;
             function updateAll() {
                 // Filters the data
                 if (price_choices.size > 0) newData = mergedData.filter((d, i) => price_choices.has(d["price"]));
-                if (rating_choices.size > 0) newData = mergedData.filter((d, i) => rating_choices.has(d["rating"]));
+                if (rating_choices.size > 0) newData = newData.filter((d, i) => rating_choices.has(d["rating"]));
                 newData = price_choices.size + rating_choices.size > 0 ? newData : mergedData;
 
                 // Paints the map
